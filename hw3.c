cf7efc59479d827dd524e7186a687f2f8b29b0e3
jeong617 <loodol99@naver.com>
2022-09-22 14:41:00 +0900

    hw3

---
+++ hw3.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int height;
+	int i, j;
+
+	printf("피라미드 몇 층?: ");
+	scanf("%d", &height);
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < height - i; j++)
+			printf(" ");
+		for (j = 0; j < 2 * i - 1; j++)
+			printf("*");
+		for (j = 0; j < height; j++)
+			printf(" ");
+		printf(" \n");
+	}
+	return 0;
+}
